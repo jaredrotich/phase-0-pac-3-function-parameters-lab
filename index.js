04d69a395b44edf6b04080354fba45c9dc48d3cb
jared <jaredrotich3@gmail.com>
2025-03-12 13:27:34 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+function introduction(name) {
+    console.log(`HI, my name is ${Jared}.`);
+}
+
+function introduction(name, language) {
+    console.log(`Hi, my name is ${Jared} and i am learning to program in ${JavaScript}.`)
+}
